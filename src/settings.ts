aa20532211284e3080467e6ebb359e7bb312cafe
MrMysterius <official.mrmysterius@gmail.com>
2022-10-20 15:25:17 +0200

    Custom Settings Mod

--- src/settings.ts
+++ src/settings.ts
@@ -1,23 +1,26 @@
 export class DayPlannerSettings {
-  customFolder: string = 'Day Planners';
+  customFolder: string = "Day Planners";
   mode: DayPlannerMode = DayPlannerMode.File;
   mermaid: boolean = false;
   notesToDates: NoteForDate[] = [];
   completePastItems: boolean = true;
   circularProgress: boolean = false;
   nowAndNextInStatusBar: boolean = false;
-  showTaskNotification: boolean = false
+  showTaskNotification: boolean = false;
   timelineZoomLevel: number = 4;
-  timelineIcon: string = 'calendar-with-checkmark'
+  timelineIcon: string = "calendar-with-checkmark";
   breakLabel: string = "BREAK";
   endLabel: string = "END";
+  createFile: boolean = true;
+  fileFormat: string = "YYYYMMDD";
+  fileName: string = "{{date}}.md"
 }
 
 export class NoteForDate {
   notePath: string;
   date: string;
 
-  constructor(notePath: string, date:string){
+  constructor(notePath: string, date: string) {
     this.notePath = notePath;
     this.date = date;
   }
@@ -28,13 +31,13 @@ export class NoteForDateQuery {
     return this.active(source) !== undefined;
   }
 
-  active(source: NoteForDate[]): NoteForDate{
+  active(source: NoteForDate[]): NoteForDate {
     const now = new Date().toDateString();
-    return source && source.filter(ntd => ntd.date === now)[0];
+    return source && source.filter((ntd) => ntd.date === now)[0];
   }
 }
-  
+
 export enum DayPlannerMode {
   File,
-  Command
+  Command,
 }
